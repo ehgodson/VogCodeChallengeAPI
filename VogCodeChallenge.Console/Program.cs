aa5fbc0d5b25b7afb714104f3a9b68814041b45e
Godwin Ehichoya <ehgodson@hotmail.com>
2021-03-26 00:22:15 +0100

    question7

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VogCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < QuestionClass.NamesList.Count())
+            {
+                Console.WriteLine(QuestionClass.NamesList[i]);
+                i++;
+            }
+        }
+    }
+
+    public static class QuestionClass
+    {
+        public static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+    }
+}
