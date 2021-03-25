413fa89cfa878307f816a44c5bad9de7b9e6fdc3
Godwin Ehichoya <ehgodson@hotmail.com>
2021-03-25 22:52:02 +0100

    question3

---
+++ VogCodeChallenge.API/Core/services.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VogCodeChallenge.API
+{
+    public class DataServices
+    {
+        public DataServices()
+        {
+            Data data = new Data();
+            Employees = new EmployeeService(data);
+        }
+
+        public EmployeeService Employees { get; }
+    }
+    public class EmployeeService
+    {
+        readonly Data _data;
+
+        public EmployeeService(Data data)
+        {
+            _data = data;
+        }
+
+        public IEnumerable<Employee> GetAll() => _data.Employees;
+        public IList<Employee> ListAll() => _data.Employees.ToList();
+    }
+}
