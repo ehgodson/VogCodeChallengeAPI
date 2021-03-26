a4e06efc10454b4edc956a03b35b4fdb20fd3b1b
Godwin Ehichoya <ehgodson@hotmail.com>
2021-03-26 01:07:40 +0100

    question8

--- VogCodeChallenge.Console/Program.cs
+++ VogCodeChallenge.Console/Program.cs
@@ -14,6 +14,20 @@ namespace VogCodeChallenge
                 Console.WriteLine(QuestionClass.NamesList[i]);
                 i++;
             }
+            Console.WriteLine("================================");
+            Console.WriteLine(QuestionClass.TESTModule(1));
+            Console.WriteLine(QuestionClass.TESTModule(5));
+            Console.WriteLine(QuestionClass.TESTModule(1.4));
+            Console.WriteLine(QuestionClass.TESTModule(7.2));
+            Console.WriteLine(QuestionClass.TESTModule("john"));
+            try
+            {
+                Console.WriteLine(QuestionClass.TESTModule(0));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 
@@ -25,5 +39,20 @@ namespace VogCodeChallenge
             "Jeffrey",
             "John",
         };
+        public static int TESTModule(int value) => value switch
+        {
+            int n when n > 1 && n < 2 => 3,
+            int n when n >= 1 && n <= 4 => value * 2,
+            int n when n > 4 => value * 3,
+            int n when n < 1 => throw new ArgumentException("Argument cannot be less than 1", "value"),
+            _ => value,
+        };
+        public static double TESTModule(double value) => value switch
+        {
+            double n when n > 1 && n < 2 => 3,
+            double n when n < 1 => throw new ArgumentException("Argument cannot be less than 1", "value"),
+            _ => value,
+        };
+        public static string TESTModule(string value) => value.ToUpper();
     }
 }
